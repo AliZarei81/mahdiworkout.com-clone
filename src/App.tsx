d1d9c6ca8371a8a296fb345108558ca0ac13cc9f
AmirManshadi <amirmanshadi.code@gmail.com>
2023-12-06 20:42:53 +0330

    feat: create root layout component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import {
 	createBrowserRouter,
 	createRoutesFromElements,
 	Route,
-	Outlet,
 } from 'react-router-dom'
+import Layout from './components/Layout'
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-		<Route path="/" element={<Outlet />}>
+		<Route path="/" element={<Layout />}>
 			<Route path="program" element={<h1>program</h1>} />
 			<Route path="about" element={<h1>about</h1>} />
 			<Route path="contact" element={<h1>contact</h1>} />
@@ -20,4 +20,4 @@ function App() {
 	return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
